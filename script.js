cd48faa8f42f0146263c67e73c72827ea451da3f
dhivakar07 <dhivakar782001@gmail.com>
2026-03-22 22:47:41 +0530

    updated commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function go() {
+  window.location.href = "https://www.facebook.com";
+}
+function twitter() {
+  window.location.href = "https://x.com/?lang=en-in";
+}
+function pinterest() {
+  window.location.href = "https://www.pinterest.com/";
+}
+function instagram() {
+  window.location.href = "https://www.instagram.com/";
+}
+function youtube() {
+  window.location.href = "https://www.youtube.com/";
+}
+function tiktok() {
+  window.location.href = "https://en.wikipedia.org/wiki/TikTok";
+}
